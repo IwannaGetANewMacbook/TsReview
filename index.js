aa7de9ecea9a12a950570fd6cf6692ec8bdebb3f
Suhyeon <01057557503a@gmail.com>
2022-05-31 19:09:48 +0900

    review 1st

---
+++ index.js
@@ -0,0 +1,33 @@
+let name1 = "su";
+let arr = [1, 2, 3, "kim", 2, "dwdwd"];
+let obj = { name: "suhyeon" };
+const test = "anyone";
+function myFunc(x, y) {
+    let outcome = x * y;
+    console.log(outcome);
+}
+myFunc(3, 2);
+const mymy = [1, true];
+let obj1 = {
+    name: "suhyeon",
+    age: 26,
+    location: "Dagu"
+};
+console.log(obj1);
+let members = [123, "kim"];
+const obj2 = {
+    name: "suhyeon",
+    age: 26,
+    isAdult: true
+};
+console.log(obj2);
+let anytype = true; // any타입은 모든 자료형 허용하는 자료형
+// narrowing
+function forNarrowing(x) {
+    let output;
+    if (typeof (x) === "number") {
+        output = x + 1;
+    }
+    return output;
+}
+console.log(forNarrowing(1));
---
+++ index.ts
@@ -0,0 +1,46 @@
+let name1: string = "su"
+let arr: (string | number)[] = [1, 2, 3, "kim", 2, "dwdwd"]
+let obj: {name?: string} = {name: "suhyeon"}
+
+type MyType = string | number
+
+const test: MyType = "anyone"
+
+function myFunc(x: number, y: number): void {
+  let outcome = x * y
+  console.log(outcome)
+}
+
+myFunc(3, 2)
+
+//array의 튜풀타입
+type MyType1 = [number, boolean]
+const mymy: MyType1= [1, true]
+
+let obj1: {[key: string]: string | number} = {
+  name: "suhyeon",
+  age: 26,
+  location: "Dagu"
+}
+console.log(obj1)
+
+let members: (string | number)[] = [123, "kim"]
+const obj2: {[key: string]: (string | number | boolean)} = {
+  name: "suhyeon",
+  age: 26,
+  isAdult: true
+}
+console.log(obj2)
+
+let anytype: any = true // any타입은 모든 자료형 허용하는 자료형
+
+// narrowing
+function forNarrowing(x: string | number) {
+  let output: number
+  if (typeof(x) === "number") {
+    output = x + 1
+  }
+  return output  
+}
+
+console.log(forNarrowing(1))
